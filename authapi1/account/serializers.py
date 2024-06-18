ee3e110fab8700b861b777ef593d28be186f129b
purvii <purvib12@gmail.com>
2024-06-18 17:22:04 +0545

    changes

---
+++ authapi1/account/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from account.models import User
+from django.utils.encoding import smart_str, force_bytes, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    #password 2 is used for confirmation of password
+    password2=serializers.CharField(style={'input_type':'password'},write_only=True)
+    class Meta:
+        model=User
+        fields=['email','name','password','password2']
+        extra_kwargs={
+            'password':{'write_only':True}
+        }
+
+#checking/validating if password and confirm password are same or not 
+    def validate(self, data):
+        password = data.get('password')
+        password2 = data.get('password2')
+        if password != password2:
+          raise serializers.ValidationError("Password and Confirm Password doesn't match")
+        return data
+
+    def create(self,validated_data):
+        validated_data.pop('password2')  # Remove password2 from validated_data
+        user = User.objects.create_user(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+    
+class UserLoginSerializer(serializers.ModelSerializer):
+    email=serializers.EmailField(max_length=255)
+    class Meta:
+        model=User
+        fields=['email','password']
+        
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=['id','email','name']
