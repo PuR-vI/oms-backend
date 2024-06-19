f78776186a8fba230cdefa502165610cb3f86477
purvii <purvib12@gmail.com>
2024-06-19 12:00:54 +0545

    change password added

--- authapi1/account/views.py
+++ authapi1/account/views.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.authentication import JWTTokenUserAuthentication
 from rest_framework.views import APIView
 from django.contrib.auth import authenticate
-from account.serializers import UserRegistrationSerializer,UserLoginSerializer,UserProfileSerializer
+from account.serializers import UserChangePasswordSerializer, UserRegistrationSerializer,UserLoginSerializer,UserProfileSerializer
 from account.renderers import UserRenderer
 
 
@@ -71,3 +71,12 @@ class UserProfileView(APIView):
     def get(self,request,format=None):
         serializer=UserProfileSerializer(request.user)
         return Response(serializer.data,status=status.HTTP_200_OK)
+    
+class UserChangePasswordView(APIView):
+    renderer_classes=[UserRenderer]
+    permission_classes=[IsAuthenticated]
+    def post(self,request,format=None):
+       serializer=UserChangePasswordSerializer(data=request.data,context={'user':request.user})
+       if serializer.is_valid(raise_exception=True):
+        return Response({'msg':'Password Changed Successfully'},status=status.HTTP_200_OK)
+       return Response(serializer.errors,status==status.HTTP_400_BAD_REQUEST)
